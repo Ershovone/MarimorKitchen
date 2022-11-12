eddfd1bdd7dd6a2a9498c20f01b96365be0cf996
Andrey Ershov <ershov.one@gmail.com>
2022-11-12 22:06:16 +0300

    init

---
+++ components/MenuTabs/MenuTabs.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../../styles/MenuTabs.module.css';
+
+const MenuTabs: React.FC = () => {
+  return (
+    <div className={styles.container} />
+  )
+}
+
+export default MenuTabs;
---
+++ components/Wallpaper/Wallpaper.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/Wallpaper.module.css';
+
+const Wallpaper: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      Here is going to be some description about<br />
+      food or something else, will see...
+    </div>
+  )
+}
+
+export default Wallpaper;
