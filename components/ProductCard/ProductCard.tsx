d484b8bf81781e7dd50378bbb863dc7615698282
Andrey Ershov <ershov.one@gmail.com>
2022-11-25 14:18:33 +0300

    dev

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -3,7 +3,18 @@ import styles from '../../styles/ProductCard.module.css';
 const ProductCard: React.FC = () => {
   return (
     <div className={styles.container}>
-      card
+      <div className={styles.content}>
+        <div className={styles.title}>THE NAME OF THIS GÜZEL
+          TAMAM TAMAM
+        </div>
+        <div className={styles.ingredients}>
+          full description of this magical yiy
+          tamam içindikeler
+        </div>
+        <div className={styles.price}>
+          1000 tl
+        </div>
+      </div>
     </div>
   )
 }
